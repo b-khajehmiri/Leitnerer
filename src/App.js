af4ae48ffaf199c883bacc53013e276c21d76c45
Bahador <b.khajehmiri@gmail.com>
2023-01-21 23:50:31 +0330

    Sign in responsivity starts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <div>
-      <h1>Firebase Auth & Context</h1>
       <AuthContextProvider>
         <Routes>
           <Route path="/" element={<SignIn />} />
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import bannerImg from "../images/bannerImg.png"
+import design from "./signIn.module.scss"
 
 const SignIn = () => {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -22,27 +25,41 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Sign in to your account</h1>
-        <p>
-          Don't have an account yet? <Link to="/signUp">Sign up.</Link>
-        </p>
+    <div className="container-lg">
+      <div className="row min-vh-100 justify-content-center align-items-center">
+        <div className="col col-md-6 col-12 d-flex justify-content-center align-items-center">
+          <div className={`card mx-4 ${design.signInForm}`}>
+            <div className="card-body">
+              <div>
+                <h1>Sign in to your account</h1>
+                <p>
+                  Don't have an account yet? <Link to="/signUp">Sign up.</Link>
+                </p>
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label>Email Address</label>
+                  <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button className="btn btn-warning">Sign In</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="col col-md-6 col-12 d-flex justify-content-center align-items-center">
+          <img src={bannerImg} alt="banner" className={`img-fluid px-5 px-md-0 figure-img ${design.bbb}`} />
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email Address</label>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button className="btn btn-warning">Sign In</button>
-      </form>
     </div>
   );
 };
