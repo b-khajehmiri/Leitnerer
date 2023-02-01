7b564d2031ec07b98893e21286a891b415c5632a
Bahador <b.khajehmiri@gmail.com>
2023-02-01 20:04:02 +0330

    Basic Table added 💪😎🚀

--- src/components/CardsTable.jsx
+++ src/components/CardsTable.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { useMemo } from "react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { UserAuth } from "../context/AuthContext";
+import { useTable } from "react-table";
 import NavBar from "./NavBar";
+import design from "./cardsTable.module.scss"
 
 const CardsTable = () => {
   const userId = window.localStorage.getItem("user");
@@ -15,25 +16,73 @@ const CardsTable = () => {
 
   const [cards, setCards] = useState([]);
 
-  async function getCards() {
-    const res = await axios.get(
-      `https://leitnerer-e8694-default-rtdb.firebaseio.com/${userId}.json`
-    );
-
-    let keys = Object.keys(res.data);
-    let values = Object.values(res.data);
-    values.map((v, index) => (v.id = keys[index]));
-    setCards(values);
-    console.log(values);
-  }
-
   useEffect(() => {
+    async function getCards() {
+      const res = await axios.get(
+        `https://leitnerer-e8694-default-rtdb.firebaseio.com/${userId}.json`
+      );
+      let keys = Object.keys(res.data);
+      let values = Object.values(res.data);
+      values.map((v, index) => (v.id = keys[index]));
+      setCards(values);
+    }
+
     getCards();
   }, []);
+
+  const Columns = [
+    { Header: "Front Side", accessor: "front" },
+    { Header: "Back Side", accessor: "back" },
+    { Header: "Deck Number", accessor: "deck" },
+  ];
+
+  console.log(cards);
+  const columns = useMemo(() => Columns, []);
+  // const data = useMemo(() => cards, []);
+
+  const cardsTable = useTable({
+    columns: columns,
+    data: cards,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    cardsTable;
 
   return (
     <>
       <NavBar navsShow={navsShow} />
+      <div className="container lg mt-5">
+        <table
+          className={`table table-responsive ${design.cardsTable}`}
+          {...getTableProps()}
+        >
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
