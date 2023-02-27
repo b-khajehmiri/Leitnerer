6e5ae6651cbbb58d970985889731c8fe29a3f4bd
Bahador <b.khajehmiri@gmail.com>
2023-02-27 19:21:29 +0330

    Change in card design

--- src/components/TrainingMechanism.jsx
+++ src/components/TrainingMechanism.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import getRandomElements from "../utils/GetRandomElements";
-import FlipCard from "./FlipCard";
+import "./flipCard.css";
 
 const TrainingMechanism = (props) => {
   let selectedTrainingCards = [];
+  const [flip, setFlip] = useState(false);
   const [cardIndex, setCardIndex] = useState(0);
 
   for (let i = 0; i < props.checkedDecks.length; i++) {
@@ -20,16 +21,11 @@ const TrainingMechanism = (props) => {
     selectedTrainingCards.length
   );
 
-  // console.table(props.checkedDecks);
-  console.log(selectedTrainingCards);
-
   return (
-    <div className="row">
-      <div
-        className="pt-2 pb-3 rounded-3 ps-3 text-dark mt-4 border-1 border border-dark"
-        style={{ backgroundColor: "#FFDB13" }}
-      >
-        <div className="mb-3 font17 d-flex flex-row justify-content-center fw-bolder">
+    <div className="row parent-element">
+      {console.table(selectedTrainingCards)}
+      <div className="pt-2 pb-3 rounded-3 ps-3 mt-4 bg-primary text-light">
+        <div className="font17 d-flex flex-row justify-content-center fw-bolder mb-2">
           <i className="fa-solid fa-circle-info mt-1 me-1"></i>
           Smart Leitnerer selected cards for training as below:
         </div>
@@ -46,34 +42,45 @@ const TrainingMechanism = (props) => {
           ))}
         </div>
       </div>
-      {/* <div className="mt-4 px-0 d-flex flex-row justify-content-center">
-        <div className="card w-75 border-primary">
-          <div className="card-header bg-primary text-light fw-bolder">
-            Front side:
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <div>{selectedTrainingCards[cardIndex].front}</div>
-              <div className="d-flex justify-content-center">
-                <button
-                  className="btn btn-primary mb-1 mt-5"
-                  onClick={() => {
-                    setCardIndex(cardIndex + 1);
-                  }}
-                >
-                  Show back side
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div> */}
-      <FlipCard/>
-      <p
-        className="d-flex justify-content-center mt-4 cursorPointer mx-2 mt-2 smallLinks"
-        onClick={() => props.setIsTraining(false)}
+      <div
+        className={` px-0 d-flex flex-column justify-content-center align-items-center mt-3 ${
+          flip ? "flip-scale-up-hor" : "flip-scale-up-hor2"
+        }`}
       >
-        <small>I want to select cards again.</small>
+        <div class={`flip-card ${!flip ? "" : "flip-card-2"}`}>
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <h4 className="fw-bolder mt-4">Front Side</h4>
+              <p className="px-4 my-3 text-start textJustified">
+                {selectedTrainingCards[cardIndex].front}
+              </p>
+            </div>
+            <div class="flip-card-back">
+              <h4 className="fw-bolder mt-4">Back Side</h4>
+              <p className="px-4 my-3 text-start textJustified">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و در
+                ستون و سطرآنچنان که لازم است
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-100 p-0 m-0 d-flex justify-content-center">
+        <button
+          className="btn btn-primary mt-3"
+          onClick={() => {
+            setFlip(!flip);
+          }}
+        >
+          Show back side
+        </button>
+        <button className="btn btn-danger mt-3 ms-5">Finish</button>
+      </div>
+      <p className="d-flex justify-content-center mt-4 cursorPointer mx-2 mt-2 smallLinks">
+        <small onClick={() => props.setIsTraining(false)}>
+          I want to select cards again.
+        </small>
       </p>
     </div>
   );
