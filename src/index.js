29a9cd30d99157166a6fc7215a1596738e6af6a5
Bahador <b.khajehmiri@gmail.com>
2023-10-08 18:17:22 +0330

    change to Vite

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/leitnerer">
     <App />
   </BrowserRouter>
 );
